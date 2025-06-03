75ff6ad2dbe66c2baa9dcacea4fc1227a8a4ab93
HassanTahir14 <hassan.tahir@exirodigital.com>
2025-06-03 19:52:50 +0500

    update modules

--- src/automation/automation.module.ts
+++ src/automation/automation.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AutomationController } from './automation.controller';
 import { AutomationService } from './automation.service';
-import { GeminiService } from '../services/hugging-face.service';
+import { HuggingFaceService } from '../services/hugging-face.service';
 import { SupabaseService } from '../services/supabase.service';
 import { N8nService } from '../services/n8n.service';
 
 @Module({
   imports: [],
   controllers: [AutomationController],
-  providers: [AutomationService, GeminiService, SupabaseService, N8nService],
+  providers: [AutomationService, HuggingFaceService, SupabaseService, N8nService],
 })
 export class AutomationModule {}
